9a40b090076ee9cffffa8307d3be6dee97200526
David Rodriguez-Jenkins <53097291+davidrodjen@users.noreply.github.com>
2020-07-25 10:37:10 -0700

    Created Standard controller (#13)

---
+++ BlogIT/Controllers/MainMediaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogIT.Controllers
+{
+    public class MainMediaController : Controller
+    {
+        public IActionResult MainMediaPage()
+        {
+            return View();
+        }
+    }
+}
