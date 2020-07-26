a0e90481a9c20167f8cb434fa3dbb5f036c8cc7e
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-07-26 15:26:25 -0700

    Renamed File, added comments and adjusted the call for View

---
+++ BlogIT/Controllers/MediaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlogIT.Models;
+
+namespace BlogIT.Controllers
+{
+    public class MediaController : Controller
+    {
+        /// <summary>
+        /// Returns the view of MainMediaPage
+        /// Must use MainMediaPage as "Function" Name
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult MainMediaPage()
+        {
+            return View();
+        }
+    }
+}
