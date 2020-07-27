756e1fe9b0dca5afa38d0b89c3657b8d65b73ce8
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-07-27 12:17:53 -0700

    Added properties

---
+++ BlogIT/Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogIT.Models
+{
+    public class BlogPost
+    {
+        public int BlogId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public string Signature { get; set; }
+    }
+}
