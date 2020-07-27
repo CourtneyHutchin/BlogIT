dcb6e54f15ff15fd4ce99e58e1625710d6caae16
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-07-27 12:27:31 -0700

    Added data annotations

--- BlogIT/Models/BlogPost.cs
+++ BlogIT/Models/BlogPost.cs
@@ -8,12 +8,18 @@ namespace BlogIT.Models
 {
     public class BlogPost
     {
+        [Key]
         public int BlogId { get; set; }
 
         public string Category { get; set; }
 
+        [StringLength(100)]
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
 
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Text is required")]
         public string Text { get; set; }
 
         public string Signature { get; set; }
