74ce90a7f35fe79fd03fe182324f6a80fb5a69a0
Courtney Hutchin <50035291+CourtneyHutchin@users.noreply.github.com>
2020-07-27 12:48:43 -0700

    Issue#11 blog model (#18)
    * Added properties
    
    * Added data annotations
    
    * Added documentation

---
+++ BlogIT/Models/BlogPost.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogIT.Models
+{
+    public class BlogPost
+    {
+        /// <summary>
+        /// Unique Identifier for each blog
+        /// </summary>
+        [Key]
+        public int BlogId { get; set; }
+
+        /// <summary>
+        /// The category of the blog
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// The Title of the blog
+        /// </summary>
+        [StringLength(100)]
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The descripton or summary of the blog
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The main content of the blog
+        /// </summary>
+        [Required(ErrorMessage = "Text is required")]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The signature on the blog
+        /// </summary>
+        public string Signature { get; set; }
+    }
+}
