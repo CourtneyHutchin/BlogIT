7d60d0373b45c19ec5b0e7c1f218ee59f1a7b33b
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-08-19 11:50:01 -0700

    Added role requirements to registration to create admin roles, @mywebsite.com

--- BlogIT/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BlogIT/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using BlogIT.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -19,6 +20,7 @@ namespace BlogIT.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
+        private const string AdminEmail = "@mywebsite.com";
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
@@ -79,7 +81,14 @@ namespace BlogIT.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    if (user.Email.ToLower().EndsWith(AdminEmail))
+                    {
+                        var roleResult = await _userManager.AddToRoleAsync(user, IdentityHelper.Admin);
+                        if (!roleResult.Succeeded)
+                        {
+                            _logger.LogInformation($"{user.Email} was not added to the Admin Role");
+                        }
+                    }
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
