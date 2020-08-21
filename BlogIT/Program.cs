aea1d51d422f9467548591533516d9324530b082
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-08-21 10:22:09 -0700

    Added NewsAPI, with Key, and set up Anchor Link

--- BlogIT/Program.cs
+++ BlogIT/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
 
 namespace BlogIT
 {
@@ -14,6 +17,35 @@ namespace BlogIT
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            // init with your API key
+            var newsApiClient = new NewsApiClient("5a6f078ea39446cda5b9698ccd4e26be");
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = "Apple",
+                SortBy = SortBys.Popularity,
+                Language = Languages.EN,
+                From = new DateTime(2018, 1, 25)
+            });
+            if (articlesResponse.Status == Statuses.Ok)
+            {
+                // total results found
+                Console.WriteLine(articlesResponse.TotalResults);
+                // here's the first 20
+                foreach (var article in articlesResponse.Articles)
+                {
+                    // title
+                    Console.WriteLine(article.Title);
+                    // author
+                    Console.WriteLine(article.Author);
+                    // description
+                    Console.WriteLine(article.Description);
+                    // url
+                    Console.WriteLine(article.Url);
+                    // published at
+                    Console.WriteLine(article.PublishedAt);
+                }
+            }
+            Console.ReadLine();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
